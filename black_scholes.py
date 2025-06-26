bb391fa2c6b444a3458a2f320dee2376b535b6fe
cjramsey <cameronramsey04@gmail.com>
2025-06-26 19:36:00 +0100

    add perpetual and gap option pricers

--- black_scholes.py
+++ black_scholes.py
@@ -1,12 +1,13 @@
 import numpy as np
 from scipy.stats import norm
+from abc import ABC, abstractmethod
 
 import greeks
-from options import EuropeanOption
+from options import EuropeanOption, PerpetualAmericanOption, GapOption
 
 # closed-form solutions for European options
 
-class BlackScholesPricer:
+class BlackScholesPricer(ABC):
 
     def __init__(self, option, S, sigma, r, q=0):
         self.option = option
@@ -15,7 +16,13 @@ class BlackScholesPricer:
         self.r = r
         self.q = q
         self.price = self.get_price()
-        
+
+    @abstractmethod
+    def get_price(self):
+        pass
+
+class EuropeanBlackScholesPricer(BlackScholesPricer):
+
     def get_price(self):
         K = self.option.K
         T = self.option.T
@@ -33,7 +40,7 @@ class BlackScholesPricer:
         elif self.option.type == "put":
             d1 = (np.log(S/K) + (r - q - (sigma**2)/2)*T)/(sigma*np.sqrt(T))
             d2 = d1 - sigma * np.sqrt(T)
-            p = K * np.exp(-r*T) * norm.cdf(-d1) - S * norm.cdf(-d2)
+            p = K * np.exp(-r*T) * norm.cdf(-d2) - S * norm.cdf(-d1)
             return p
         
     @property
@@ -61,18 +68,68 @@ class BlackScholesPricer:
         }
 
         return greeks_dict
+    
+class PerpetualAmericanBlackSholesPricer(BlackScholesPricer):
+
+    def get_price(self):
+        K = self.option.K
+        S = self.S
+        r = self.r
+        q = self.q
+        sigma = self.sigma
+
+        w = r - q - (sigma**2)/2
+        if self.option.type == "call":
+            alpha = (-w + np.sqrt(w**2 +2*sigma**2*r))/sigma**2
+            H = K*alpha/(alpha - 1)
+            if S < H:
+                c = K/(alpha - 1) * (((alpha - 1)*S)/(alpha*K))**alpha
+            else: 
+                c = S - K
+            return c
         
+        elif self.option.type == "put":
+            alpha = (w + np.sqrt(w**2 + 2*sigma**2*r))/sigma**2
+            H = K*alpha/(alpha + 1)
+            if S > H:
+                p = K/(alpha + 1) * (((alpha+1)*S)/(alpha*K))**(-alpha)
+            else:
+                p = K - S
+            return p
+    
+class GapBlackScolesPricer(BlackScholesPricer):
+
+    def get_price(self):
+        K1 = self.option.K1
+        K2 = self.option.K2
+        T = self.option.T
+        S = self.S
+        r = self.r
+        q = self.q
+        sigma = self.sigma
+        
+        d1 = (np.log(S/K2) + (r - q + (sigma**2)/2)*T)/(sigma*np.sqrt(T))
+        d2 = d1 - sigma*np.sqrt(T)
+
+        if self.option.type == "call":
+            c = S*np.exp(-q*T)*norm.cdf(d1) - K1*np.exp(-r*T)*norm.cdf(d2)
+            return c
+        
+        elif self.option.type == "put":
+            p = K1*np.exp(-r*T)*norm.cdf(-d2) - S*np.exp(-q*T)*norm.cdf(-d1)
+            return p
 
 if __name__ == "__main__":
     
-    K = 95
+    K1 = 400000
+    K2 = 350000
     T = 1
-    type = "call"
-    S = 100
-    sigma = 0.1
+    type = "put"
+    S = 500000
+    sigma = 0.2
     r = 0.05
+    q = 0
 
-    option = EuropeanOption(K, T, type)
-    pricer = BlackScholesPricer(option, S, sigma, r)
+    option = GapOption(K1, K2, 1, type)
+    pricer = GapBlackScolesPricer(option, S, sigma, r, q)
     print(pricer.price)
-    print(pricer.greeks)
