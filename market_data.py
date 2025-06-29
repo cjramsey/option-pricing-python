0b05305800283a429714d16b3a18ec8280b23832
cjramsey <cameronramsey04@gmail.com>
2025-06-29 15:10:55 +0100

    add risk-free-rate scraping and separate calls/puts

--- market_data.py
+++ market_data.py
@@ -1,48 +1,50 @@
 import datetime
 import os
 import pandas as pd
+import pandas_datareader as pdr
 import numpy as np
 import yfinance as yf
 
 class MarketData:
 
-    def __init__(self, tickers, start=None, end=None, interval=None,
-                save_stock_data=False, save_option_data=False):
+    def __init__(self, tickers, save_stock_data=False, save_option_data=False):
         self.tickers = tickers
-        self.start = start if start else datetime.datetime(2025, 1, 1)
-        self.end = end if end else datetime.datetime.today()
-        self.interval = interval
         self.save_stock_data = save_stock_data
         self.save_option_data = save_option_data
-        self.stocks = self.get_stock_data()
-        self.options = self.get_options_data()
+        self.stocks = {}
+        self.calls = {}
+        self.puts = {}
+
+        self.get_stock_data()
+        self.get_options_data()
 
         if save_stock_data or save_option_data:
             self.save_data()
 
     def get_stock_data(self):
-        self.stocks = {}
         for ticker in self.tickers:
             tk = yf.Ticker(ticker)
             self.stocks.update({f"{ticker}" : tk.history(period="max")})
-        return self.stocks
 
     def get_options_data(self):
-        self.options = {}
         for ticker in self.tickers:
             tk = yf.Ticker(ticker)
             exps = tk.options
-            options = pd.DataFrame()
+            calls, puts = pd.DataFrame(), pd.DataFrame()
             for e in exps:
                 opt = tk.option_chain(e)
-                opt = pd.concat([opt.calls, opt.puts])
-                opt["expirationDate"] = e
-                options = pd.concat([options, opt], ignore_index=True)
-            options.drop(["change", "percentChange", "openInterest", 
-                        "inTheMoney", "contractSize", "currency"], 
-                        axis=1, inplace=True)
-            self.options.update({ticker: options})
-        return self.options
+                opt.calls["expirationDate"] = e
+                opt.puts["expirationDate"] = e
+                calls = pd.concat([calls, opt.calls], ignore_index=True)
+                puts = pd.concat([puts, opt.puts], ignore_index=True)
+
+            drop_columns = ["change", "percentChange", "inTheMoney", 
+                            "contractSize", "currency"]
+            calls.drop(drop_columns, axis=1, inplace=True)
+            puts.drop(drop_columns, axis=1, inplace=True)
+            
+            self.calls.update({ticker: calls})
+            self.puts.update({ticker: puts})
 
     def save_data(self):
         base_dir = os.path.dirname(__file__)
@@ -52,15 +54,19 @@ class MarketData:
             pass
         if self.save_stock_data:
             for ticker in self.tickers:
-                file_name1 = f"{ticker}_stock_{datetime.date.today()}.csv"
-                file_path1 = os.path.join(base_dir, "data", file_name1)
-                self.stocks[ticker].to_csv(file_path1)
+                file_name = f"{ticker}_stock_{datetime.date.today()}.csv"
+                file_path = os.path.join(base_dir, "data", file_name)
+                self.stocks[ticker].to_csv(file_path)
             
         if self.save_option_data:
             for ticker in self.tickers:
-                file_name2 = f"{ticker}_options_{datetime.date.today()}.csv"
-                file_path2 = os.path.join(base_dir, "data", file_name2)
-                self.options[ticker].to_csv(file_path2)
+                file_name = f"{ticker}_calls_{datetime.date.today()}.csv"
+                file_path = os.path.join(base_dir, "data", file_name)
+                self.calls[ticker].to_csv(file_path)
+
+                file_name = f"{ticker}_puts_{datetime.date.today()}.csv"
+                file_path = os.path.join(base_dir, "data", file_name)
+                self.puts[ticker].to_csv(file_path)
 
     @staticmethod
     def parse_file_name(file_name, delimiter="_"):
@@ -69,12 +75,21 @@ class MarketData:
 
     def load_data(self, file_names=None):
         base_dir = os.path.dirname(__file__)
+        data_dir = os.path.join(base_dir, "data")
         if not file_names:
-            for entry in os.scandir(base_dir):
-                if entry.is_file():
-                    df = pd.read_csv(entry.path, index_col=[0])
+            for entry in os.scandir(data_dir):
+                if not entry.is_file():
+                    continue
+                df = pd.read_csv(entry.path, index_col=[0])
+                ticker, type, *_ = self.parse_file_name(entry.name)
+                if type == "stock":
+                    self.stocks.update({ticker: df})
+                elif type == "calls":
+                    self.calls.update({ticker: df})
+                elif type == "puts":
+                    self.puts.update({ticker: df})
             return
-            
+
         for file_name in file_names:
             path = os.path.join(base_dir, "data", file_name)
             try:
@@ -85,11 +100,46 @@ class MarketData:
                 ticker, type, *_ = self.parse_file_name(file_name)
             if type == "stock":
                 self.stocks.update({ticker: df})
-            elif type == "options":
-                self.options.update({ticker: df})
-                    
+            elif type == "call":
+                self.calls.update({ticker: df})
+            elif type == "puts":
+                self.puts.update({ticker: df})
+
+
+def get_risk_free_rates(start=None, durations=None):
+    start = start if start else datetime.datetime(2025, 5, 1)
+    durations =  durations if durations else ["DGS1MO", "DGS3MO", "DGS6MO", 
+                                              "GS1", "GS2", "GS3"]
+
+    risk_free_rates = pdr.DataReader("DGS1MO", "fred", start)
+    for duration in durations[1:]:
+        risk_free_rates = pd.merge(risk_free_rates, pdr.DataReader(duration, "fred", start), 
+                         left_index=True, right_index=True, how="outer")
+
+    sofr = pdr.DataReader("SOFR", "fred", start)
+    risk_free_rates = pd.merge(sofr, risk_free_rates, left_index=True, 
+                               right_index=True, how="inner")
+
+    risk_free_rates.rename(columns={"DGS1MO": 1/12, "DGS3MO": 1/4, "DGS6MO": 1/2, 
+                                    "GS1": 1, "GS2": 2, "GS3": 4, "SOFR": 3/365}, 
+                                    inplace=True)
+    
+    risk_free_rates = risk_free_rates.interpolate(method="linear").dropna().round(2)
+
+    risk_free_rates.index = risk_free_rates.index.date
+    
+    return risk_free_rates
+
+                   
+def main():
+    tickers = ["AAPL", "MSFT", "SPY", "NVDA", "PLTR",
+               "AMZN", "GOOG", "TSLA", "TSM", "TQQQ"]
+    #data = MarketData([])
+    #data.load_data()
+    #print(isinstance(data.calls["AMZN"], pd.DataFrame))
+    x = get_risk_free_rates()
+    print(x)
 
 if __name__ == "__main__":
-    pass
-
+    main()
 
